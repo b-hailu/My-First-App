7ed8ada91ffc60a131b23142604d197be2797264
bhailu <bhailu103@gmail.com>
2018-12-31 14:19:21 -0600

    project about house

--- Program.cs
+++ Program.cs
@@ -2,11 +2,27 @@
 
 namespace classwork
 {
+    //class is a blue print of an object.
     class Program
+    //has no access modifier.if ther is no access modefier it is a default.
     {
+
+
         static void Main(string[] args)
-        {
+        {//static  means no copies it is only one.
             Console.WriteLine("Hello World!");
+            /* console is a class and writeline is a method with a 
+            parameter*/
+            printer myPrinter= new printer();
+            myPrinter.ConnectCable = "connect";
+            myPrinter.Cablewired();
+            Console.WriteLine(myPrinter.ConnectCable);
+    
+
+
+
+
         }
+
     }
 }
