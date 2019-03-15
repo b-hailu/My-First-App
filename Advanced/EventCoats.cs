6ae2d4226dd56013eac65d692e1d0a056816018e
Belaynesh <bhailu103@gmail.com>
2019-03-15 09:21:50 -0500

    Advanced C#

---
+++ Advanced/EventCoats.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using classwork;
+
+namespace Advanced
+{
+    public delegate string MyCoat(string message);
+
+    public class Coats
+    {
+        public event MyCoat MyEventCoat; // there is no underscore b/c it is not private
+        public string MyResult { get; private set; }
+        //default constructor
+        public Coats()
+        {
+            this.MyEventCoat += new MyCoat(this.MyFavoriteCoat);
+            MyResult = MyEventCoat("Trench");
+        }
+
+        public string MyFavoriteCoat(string message)
+        {
+            return " My Favorite Coat is " + message;
+        }
+    }
+}
